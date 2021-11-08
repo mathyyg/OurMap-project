8e144446f66aac84a6ecfcf5e9bc615493b13050
RBbastien <92052616+RBbastien@users.noreply.github.com>
2021-11-08 18:51:11 +0100

    3.2.5 Marqueur n'est plus une classe abstraite

--- src/main/java/org/jolmkbL2B/marqueurs/Marqueur.java
+++ src/main/java/org/jolmkbL2B/marqueurs/Marqueur.java
@@ -4,7 +4,7 @@ import org.jxmapviewer.viewer.DefaultWaypoint;
 import org.jxmapviewer.viewer.GeoPosition;
 /** Les methodes sont des getters et setters, l'objet en lui meme ne realsie pas vraiment d'action autre que le partage
  * des informations qu'il contient. Idem pour les classes étendues */
-public abstract class Marqueur extends DefaultWaypoint {
+public class Marqueur extends DefaultWaypoint {
     private PlaceType placeType; //type d'endroit. Redondant avec le instanceof ?
     private long lieuID;        //identifiant dans la base de données
     private String name;        // nom de l'endroit
