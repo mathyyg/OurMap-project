923e5be59f4560e2fca02a6acda3d513455ab12a
mathys <mathys.gagner@etu.univ-nantes.fr>
2021-11-15 18:31:12 +0100

    Rename AppliHub -> AppliHubFrame - Mathys #6

--- src/main/java/org/jolmkbL2B/vue/AppliHubFrame.java
+++ src/main/java/org/jolmkbL2B/vue/AppliHubFrame.java
@@ -1,18 +1,14 @@
 package org.jolmkbL2B.vue;
 
-import com.formdev.flatlaf.FlatDarkLaf;
-import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.intellijthemes.FlatDarkPurpleIJTheme;
-import org.jolmkbL2B.App;
-import org.jolmkbL2B.Main;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class AppliHub extends JFrame {
+public class AppliHubFrame extends JFrame {
     JPanel principal;
 
-    public AppliHub() {
+    public AppliHubFrame() {
         super("HOME");
 //        principal.add();
         this.setContentPane(principal);
@@ -22,7 +18,7 @@ public class AppliHub extends JFrame {
 
     public static void main(String[] args) {
         FlatDarkPurpleIJTheme.setup();
-        AppliHub ah = new AppliHub();
+        AppliHubFrame ah = new AppliHubFrame();
         ah.pack();
         ah.setVisible(true);
     }
