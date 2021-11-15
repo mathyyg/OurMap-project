038192c3727cdfd4101867d38790ea08d6328d3c
mathgyy <mathgyy@gmail.com>
2021-11-15 15:39:11 +0100

    Revert "Version 12/11/2021"

--- src/main/java/org/jolmkbL2B/Main.java
+++ src/main/java/org/jolmkbL2B/Main.java
@@ -1,93 +1,86 @@
 package org.jolmkbL2B;
-import java.sql.*;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import javax.swing.event.MouseInputListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
+import java.awt.BorderLayout;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+
+import javax.swing.event.MouseInputListener;
 import org.jxmapviewer.input.PanKeyListener;
 import org.jxmapviewer.input.PanMouseInputListener;
 import org.jxmapviewer.input.ZoomMouseWheelListenerCursor;
 import org.jxmapviewer.input.CenterMapListener;
+
 import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.cache.FileBasedLocalCache;
+
 import org.jxmapviewer.painter.*;
+
 import org.jxmapviewer.viewer.*;
-import org.jolmkbL2B.marqueurs.*;
+
+import org.jolmkbL2B.locations.*;
+import org.jolmkbL2B.markers.*;
 
 
 public class Main
 {
-    public static void main( String[] args ) {
-        System.out.println("Hello World!");
-        App app = new App();
-
-
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
         //Generator generator = new Generator();
+
         JXMapViewer OurMap = new JXMapViewer();
-        MarqueurLoader loader = new MarqueurLoader();
-        Set<Waypoint> markers = new HashSet<Waypoint>();
+
 
 
         /** Creer une TileFactoryInfo pour importer OpenStreetMap La Tile factory genere grosso modo topus les points de la carte */
-        TileFactoryInfo info = new OSMTileFactoryInfo();
+        TileFactoryInfo info= new OSMTileFactoryInfo();
         DefaultTileFactory tileFactory = new DefaultTileFactory(info);
         OurMap.setTileFactory(tileFactory);
 
+        boolean[] tab = {true, true, false, false};
+        Hotel almostAnHotel = new Hotel(47.590313485235505, 1.3355955057336704, "41000prkrndhjsns", "Je c c pas 1 hotel",
+                "Blois", "41000", "Jean jaures", true, "zzzzzzzzzzt", "www.google.com",
+                "mdr", 5, tab, true);
 
-        /** AFFICHARGE MARQUEUR */
-        markers.add(sncf);
-        markers.add(papin);
+        HotelMarker monBeauMarqueur = new HotelMarker(almostAnHotel);
+
+        DefaultWaypoint jaures = new DefaultWaypoint(new GeoPosition(47.590313485235505, 1.3355955057336704));
+/** AFFICHARGE MARQUEUR */
+        Set<Waypoint> markers = new HashSet<Waypoint>();
+            markers.add(jaures);
 
         WaypointPainter<Waypoint> waypointPainter = new WaypointPainter<Waypoint>();
-        waypointPainter.setWaypoints(markers);
+            waypointPainter.setWaypoints(markers);
 
         List<Painter<JXMapViewer>> painters = new ArrayList<Painter<JXMapViewer>>();
-        painters.add(waypointPainter);
+            painters.add(waypointPainter);
 
         CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>(painters);
+
         OurMap.setOverlayPainter(painter);
 
 
-        /** mouse clicks to waypoints method  */
-        OurMap.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent me) {
-
-                /** création d'un marqueur en cliquant  */
-                JXMapViewer me_src = (JXMapViewer) me.getSource();
-
-                DefaultWaypoint clickwaypoint = new DefaultWaypoint((me_src.convertPointToGeoPosition(me.getPoint())));
-
-                markers.add(clickwaypoint);
-                WaypointPainter<Waypoint> waypointPainter = new WaypointPainter<Waypoint>();
-                waypointPainter.setWaypoints(markers);
-
-                List<Painter<JXMapViewer>> painters = new ArrayList<Painter<JXMapViewer>>();
-                painters.add(waypointPainter);
-
-                CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>(painters);
-                OurMap.setOverlayPainter(painter);
-
-
-                System.out.println(" mouse  x coordinates =" + me_src.getMousePosition().getX() + "/ mouse y coordinates =" + me_src.getMousePosition().getY());
-                System.out.println("CONVERTING MOUSE COORDINATES TO geoposition ones =====> latitude :" + me_src.convertPointToGeoPosition(me.getPoint()).getLatitude() + "  longitude :" + me_src.convertPointToGeoPosition(me.getPoint()).getLongitude());
-
-                /**address location or center  position */
-
-
-            }
-        });
-
-        /** Interactions  avec le bouton de la souris*/
+        /** Interactions */
         MouseInputListener mia = new PanMouseInputListener(OurMap);
         OurMap.addMouseListener(mia);
         OurMap.addMouseMotionListener(mia);
+
         OurMap.addMouseListener(new CenterMapListener(OurMap));
+
         OurMap.addMouseWheelListener(new ZoomMouseWheelListenerCursor(OurMap));
+
         OurMap.addKeyListener(new PanKeyListener(OurMap));
 
         /** Affichage de la carte */
@@ -96,7 +89,10 @@ public class Main
         mapFrame.setSize(800, 600);
         mapFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mapFrame.setVisible(true);
-        OurMap.setAddressLocation(gare.getCoord());
+        OurMap.setAddressLocation(almostAnHotel.getCoord());
         OurMap.setZoom(7);
+
+        System.out.println(almostAnHotel.toString());
+
     }
 }
--- src/main/java/org/jolmkbL2B/controllers/MarqueurController.java
+++ src/main/java/org/jolmkbL2B/controllers/MarqueurController.java
@@ -1,102 +1,31 @@
 package org.jolmkbL2B.controllers;
 import org.jolmkbL2B.marqueurs.*;
 
-import java.sql.ResultSet;
+import java.util.HashSet;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-/** Cette classe contient toutes les méthodes pour les interactions avec la base de données concernant les marqueurs et
- * objets dérivés.
- * @author Bastien*/
+
 public class MarqueurController {
-    private Connection con;
+    protected final Connection con;
+    private HotelController hotelController;
 
     public MarqueurController(Connection con) {
         this.con = con;
-    }
-
-    
-    public ResultSet fetchAll() {
         try {
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM marqueurs;");
+            stmt.execute("USE ourmapdb;");
             stmt.close();
-            return rs;
-        }
-        catch(SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+            this.hotelController = new HotelController(this.con);
 
-    public ResultSet fetchMarqueurBasicInfo(long id)    {
-        try {
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM marqueurs WHERE idmarqueur = " + id + ";");
-            checkResultSetSize(rs);
-            stmt.close();
-            return rs;
-        }
-        catch(SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public ResultSet[] fetchAllInfo(long id, PlaceType placeType) {
-        String table = selectTable(placeType);
-
-        try {
-            if (table == "none")    return null;
-            else {
-                Statement stmt = con.createStatement();
-                ResultSet infoMarqueur = stmt.executeQuery("SELECT * FROM marqueurs NATURAL JOIN " + table + " WHERE " +
-                        "idmarqueur = " + id + ";");
-                checkResultSetSize(infoMarqueur);
-                ResultSet commentaires = stmt.executeQuery("SELECT idmarqueur, idutilisateur, text, FROM commentaires " +
-                        "WHERE idmarqueur = " + id + " AND setVisible = 0;");
-                stmt.close();
-                ResultSet[] rsTab = {infoMarqueur, commentaires};
-                return rsTab;
-            }
-        }
-        catch(SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public boolean updateInfo(PlaceType placeType){
-        boolean success = false;
-        String table = selectTable(placeType);
-        return success;
-
-    }
-    private void checkResultSetSize(ResultSet rs)   {
-        try {
-            int rsSize = rs.getFetchSize();
-            if (rsSize > 1)
-            {
-                System.out.println(" Warning : Unexpected ResultSet size. " +
-                        "Exepected : 1. Actual size : " + rsSize + ".\nStill returning all results.");
-            }
-        }
-        catch(SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    private String selectTable(PlaceType placeType) {
-        switch(placeType)   {
-            case SCHOOL:
-                return "schools";
-            case HOTEL:
-                return "hotels";
-            case ARRETBUS:
-                return "arretsbus";
-            default:
-                System.err.println("Argument PlaceType non reconnu;");
-                return "none";
-        }
+
+    public HashSet<Marqueur> getAllMarkers()    {
+
     }
+
 }
