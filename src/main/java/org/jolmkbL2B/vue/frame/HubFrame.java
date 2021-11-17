7e8bceeb712f9e885802c74c0fe2c4a202219b1e
mathys <mathys.gagner@etu.univ-nantes.fr>
2021-11-17 20:33:42 +0100

    Add MarqueurInfosPanel + HubFrame(test purposes) + JFormDesigner forms - Mathys #14 - 2.3

---
+++ src/main/java/org/jolmkbL2B/vue/frame/HubFrame.java
@@ -0,0 +1,28 @@
+/*
+ * Created by JFormDesigner on Wed Nov 17 20:32:14 CET 2021
+ */
+
+package org.jolmkbL2B.vue.frame;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author Mathys Gagner
+ */
+public class HubFrame extends JPanel {
+    public HubFrame() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        setLayout(new BorderLayout());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
