a29f2b21e98cdebbc48e49086046c1788406abd1
mathys <mathys.gagner@etu.univ-nantes.fr>
2021-12-01 14:21:43 +0100

    Fix MarqueurController - Mathys - 2.5.2

--- src/main/java/org/jolmkbL2B/controllers/MarqueurController.java
+++ src/main/java/org/jolmkbL2B/controllers/MarqueurController.java
@@ -28,13 +28,6 @@ public class MarqueurController  {
                     ". Initialization cannot continue.");
             // Soucis : si pas internet, on peut pas utiliser l'appli
             sqlException.printStackTrace();
-            try {
-                con.close();
-            }
-            catch(SQLException e)   {
-                e.printStackTrace();
-                System.err.println("Connection cannot be closed or already is.");
-            }
         }
     }
 
