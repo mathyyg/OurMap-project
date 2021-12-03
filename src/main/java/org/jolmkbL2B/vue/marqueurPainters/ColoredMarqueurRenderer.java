6eab1e5c45b7f6ff099e19e482d2aea95458700c
khaled <khaled.mahdi@etu.univ-tours.fr>
2021-12-03 14:55:32 +0200

    2.6.6

--- src/main/java/org/jolmkbL2B/vue/marqueurPainters/ColoredMarqueurRenderer.java
+++ src/main/java/org/jolmkbL2B/vue/marqueurPainters/ColoredMarqueurRenderer.java
@@ -1,41 +1,125 @@
-//package org.jolmkbL2B.vue.marqueurPainters;
-//
-//import java.awt.Color;
-//import java.awt.FontMetrics;
-//import java.awt.Graphics2D;
-//import java.awt.geom.Point2D;
-//import java.awt.image.BufferedImage;
-//import java.net.URL;
-//import java.rmi.ServerError;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import javax.imageio.ImageIO;
-//
-//import org.jolmkbL2B.marqueurs.Marqueur;
-//import org.jxmapviewer.JXMapViewer;
-//import org.jxmapviewer.viewer.WaypointRenderer;
-//
-//public class ColoredMarqueurRenderer implements WaypointRenderer<Marqueur> {
-//
-//    private final Map<Color, BufferedImage> map = new HashMap<Color, BufferedImage>();
-//    private BufferedImage origImage;
-//
-//    public ColoredMarqueurRenderer() {
-//            URL resource = getClass().getResource("waypoint_white.png");
-//
-//            try
-//            {
-//                origImage = ImageIO.read(resource);
-//            }
-//            catch (Exception ex)
-//            {
-//                System.err.println("couldn't read waypoint_white.png");
-//            }
-//        }
-//        paintWaypoint(Graphics2D g, JXMapViewer OurMap, Marqueur marqueur)  {
-//
-//
-//        }
-//    }
-//}
+package org.jolmkbL2B.vue.marqueurPainters;
+
+
+import org.jolmkbL2B.marqueurs.MarqueurLoader;
+import  org.jolmkbL2B.vue.panel.MapPanel;
+import java.awt.Color;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.rmi.ServerError;
+import java.sql.ResultSet;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.sql.*;
+import java.util.Objects;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageInputStream;
+
+import org.jolmkbL2B.marqueurs.ArretBus;
+import org.jolmkbL2B.marqueurs.Marqueur;
+import org.jolmkbL2B.marqueurs.*;
+import org.jolmkbL2B.marqueurs.MarqueurLoader.*;
+import org.jolmkbL2B.marqueurs.PlaceType;
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.viewer.Waypoint;
+import org.jxmapviewer.viewer.WaypointRenderer;
+
+
+import static org.jolmkbL2B.marqueurs.Marqueur.*;
+/**
+ *
+ *
+ *
+ *
+ * */
+
+public class ColoredMarqueurRenderer implements WaypointRenderer<Marqueur> {
+
+
+    BufferedImage BLUEPIN;
+
+    {
+        try {
+            BLUEPIN = ImageIO.read(new File("C:\\Users\\Alfa-Tech\\IdeaProjects\\OurMap-project\\src\\main\\java\\org\\jolmkbL2B\\vue\\marqueurPainters\\BLUE PIN(1).png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    BufferedImage REDPIN;
+
+    {
+        try {
+
+
+           REDPIN =ImageIO.read(new File("C:\\Users\\Alfa-Tech\\IdeaProjects\\OurMap-project\\src\\main\\java\\org\\jolmkbL2B\\vue\\marqueurPainters\\RED PIN(1).png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    BufferedImage ORANGEPIN;
+
+    {
+        try {
+
+
+            ORANGEPIN =ImageIO.read(new File("C:\\Users\\Alfa-Tech\\IdeaProjects\\OurMap-project\\src\\main\\java\\org\\jolmkbL2B\\vue\\marqueurPainters\\ORANGE PIN.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+    @Override
+    public void paintWaypoint(Graphics2D g, JXMapViewer map, Marqueur waypoint)
+    {
+
+        if (waypoint.getPlaceType()==PlaceType.HOTEL)
+        {
+            Point2D point = map.getTileFactory().geoToPixel(waypoint.getPosition(), map.getZoom());
+
+            int x = (int) point.getX() - REDPIN.getWidth() / 2;
+            int y = (int) point.getY() - REDPIN.getHeight();
+
+            g.drawImage(REDPIN, x, y, null);
+        }
+        else if (waypoint.getPlaceType()==PlaceType.ARRETBUS)
+        {
+            Point2D point = map.getTileFactory().geoToPixel(waypoint.getPosition(), map.getZoom());
+
+            int x = (int) point.getX() - BLUEPIN.getWidth() / 2;
+            int y = (int) point.getY() - BLUEPIN.getHeight();
+
+            g.drawImage(BLUEPIN, x, y, null);
+        }
+
+        else if (waypoint.getPlaceType()==PlaceType.SCHOOL)
+        {
+            Point2D point = map.getTileFactory().geoToPixel(waypoint.getPosition(), map.getZoom());
+
+            int x = (int) point.getX() - ORANGEPIN.getWidth() / 2;
+            int y = (int) point.getY() - ORANGEPIN.getHeight();
+
+            g.drawImage(ORANGEPIN, x, y, null);
+        }
+
+
+
+    }
+
+
+
+}
