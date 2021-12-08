d29a73f34fb5b2d538c1f184d965419ada9fb98e
RBbastien <aranirxolker@gmail.com>
2021-12-08 21:33:18 +0100

    Javadoc Updates

--- src/main/java/org/jolmkbL2B/controllers/CommentaireController.java
+++ src/main/java/org/jolmkbL2B/controllers/CommentaireController.java
@@ -2,16 +2,27 @@ package org.jolmkbL2B.controllers;
 
 import java.sql.*;
 
+/** Cette classe permet de gérer les interactions entre l'application et la base de données pour la gestion des
+ * commentaires.
+ * @author Bastien Richardeau*/
 public class CommentaireController implements Commentaires  {
     private Connection con;
 
+    /** Constructeur. Etablit la connection à la base de données sur un serveur distant
+     * @throws SQLException en cas de soucis de connection.
+     */
     public CommentaireController()    {
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://play.kidl.fr:3306/?user=mathys",
                     "mathys", "projet2021GL");
             this.con = con;
+            /** Requête sur le serveur pour utiliser la base de données de
+             dédiée à l'application */
             Statement stmt = con.createStatement();
             stmt.execute("USE ourmapdb;");
+            /** Les updates et insertion sur la base de données ne sont pas enregistrées
+             * automatiquement. Il est nécessaire d'executer la commande "con.commit(); pour enregistrer les changements.
+             * Ceci permet d'éviter que des transactions incompletes soient enregistrés en cas de crash.*/
             con.setAutoCommit(false);
             stmt.close();
         }
@@ -25,6 +36,12 @@ public class CommentaireController implements Commentaires  {
         }
     }
 
+    /** Cette méthode renvoit un ResultSet contenant les commentaires qui ont été déposés sur un marqueur, pourvu que la
+     * valeur de la colonne "setVisible" soit à 1
+     * @author Bastien Richardeau
+     * @param idmarqueur (identifiant du marqueur pour lequel on souhaite voir les commentaires.
+     * @return  un ResultSet avec les collones suivantes (idmarqueur, idutilisateur, text)
+     * @throws SQLException*/
     public ResultSet fetchAllCommentairesByMarqueur(long idmarqueur)  {
         try {
             Statement stmt = con.createStatement();
@@ -37,6 +54,11 @@ public class CommentaireController implements Commentaires  {
         return null;
     }
 
+    /** Cette méthode renvoit un ResultSet contenant les commentaires qui ont été déposés par un utilisateur donné
+     * @author Bastien Richardeau
+     * @param idutilisateur (identifiant d'un utilisateur).
+     * @return  un ResultSet avec les collones suivantes (idmarqueur, idutilisateur, text)
+     * @throws SQLException*/
     public ResultSet fetchAllCommentairesByUser(long idutilisateur)  {
         try {
             Statement stmt = con.createStatement();
@@ -50,18 +72,32 @@ public class CommentaireController implements Commentaires  {
         return null;
     }
 
+
+    /** Cette méthode permet de montrer ou cacher les commentaires d'un d'utilisateur sur un marqueur Pour cela, elle traduit
+     * un booleen java en 0 ou 1 afin de pouvir l'entrer dans la base de données. Elle effectue ensuite une requete de
+     * modification. Si celle ci réussi, la méthode renvoie true, sinon elle renvoie false
+     * @author Bastien Richardeau
+     * @param idutilisateur identifiant de l'utilisateur ayant posté le commentaire à masquer / montrer
+     * @param idmarqueur identifiant du marqueur sur lequel se trouvent les commentaires à masquer / montrer
+     * @param visible true si on souhaite montrer un commentaire, false si on se souhaite le cacher*/
     public boolean setCommentaireVisible(long idutilisateur, long idmarqueur, boolean visible) {
-        //On s'assure que le booleen sera bien transmis sous lq forme 0 ou 1 (la BD etant configuree pour que les booleens soient codés sur 1 bit
+        /**On s'assure que le booleen sera bien transmis sous lq forme 0 ou 1 (la BD etant configuree
+         * pour que les booleens soient sous forne de 0 ou 1)
+         */
         int isVisible = 0;
         if(visible == true) isVisible = 1;
         try {
             Statement stmt = con.createStatement();
-            stmt.executeUpdate("UPDATE `ourmapdb`.`commentaires`\n" +
+            int success = stmt.executeUpdate("UPDATE `ourmapdb`.`commentaires`\n" +
                     "(SET `setVisible`= " + isVisible + ")\n" +
                     "WHERE idmarqueur = " + idmarqueur + "AND idutlisateur = " + idutilisateur + ";\n");
             stmt.close();
-            con.commit();
-            return true;
+            con.commit(); /** Enregistrement de la modification, le setAutoCommit étant sur false. */
+            if(success == 0) { /** Si aucune ligne de la BD n'a été changée, retourner false */
+                System.err.println("Aucun commentaire n'a pu être modifié.");
+                return false;
+            }
+            else return true; /** Si une ou plusieurs lignes de la BD ont été changées, retourner true)*/
         }
         catch(SQLException e) {
             e.printStackTrace();
@@ -69,10 +105,17 @@ public class CommentaireController implements Commentaires  {
         return false;
     }
 
+
+    /** Cette méthode permet de poster un commentaire sur un marqueur avec une requête SQL
+     * @author Bastien Richardeau
+     * @param idmarqueur Marqueur sur lequel on poste le commentaire
+     * @param idutilisateur identifiant de l'utilisateur postant le commentaire
+     * @param text Contenu du commentaire
+     * @return true si le commentaire a été ajouté, false sinon*/
     public boolean postCommentaire(long idutilisateur, long idmarqueur, String text)  {
         try {
             Statement stmt = con.createStatement();
-            stmt.executeUpdate("INSERT INTO `ourmapdb`.`commentaires`\n" +
+            int success = stmt.executeUpdate("INSERT INTO `ourmapdb`.`commentaires`\n" +
                     "(`idmarqueur`,\n" +
                     "`idutilisateur`,\n" +
                     "`text`,\n" +
@@ -84,12 +127,13 @@ public class CommentaireController implements Commentaires  {
                     " 1);\n");
 
             stmt.close();
-            con.commit();
-            return true;
+            con.commit(); /** Enregistrement de la modification, le setAutoCommit étant sur false. */
+            if(success > 0) return true;
         }
         catch(SQLException e) {
             e.printStackTrace();
         }
+        System.err.println("Aucun commentaire n'a été ajouté.");
         return false;
     }
 }
--- src/main/java/org/jolmkbL2B/controllers/ListeController.java
+++ src/main/java/org/jolmkbL2B/controllers/ListeController.java
@@ -5,13 +5,20 @@ import java.sql.*;
 public class ListeController implements Listes {
     private Connection con;
 
+    /** Constructeur. Etablit la connection à la base de données sur un serveur distant
+     * @throws SQLException en cas de soucis de connection.
+     */
     public ListeController()    {
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://play.kidl.fr:3306/?user=mathys",
                     "mathys", "projet2021GL");
             this.con = con;
             Statement stmt = con.createStatement();
+            /* Requête sur le serveur pour utiliser la base de données de dédiée à l'application */
             stmt.execute("USE ourmapdb;");
+            /* Les updates et insertion sur la base de données ne sont pas enregistrées
+             * automatiquement. Il est nécessaire d'executer la commande "con.commit(); pour enregistrer les changements.
+             * Ceci permet d'éviter que des transactions incompletes soient enregistrés en cas de crash.*/
             con.setAutoCommit(false);
             }
         catch(SQLException sqlException) {
@@ -24,13 +31,25 @@ public class ListeController implements Listes {
         }
     }
 
+    /** Cette méthode renvoie un ResultSet contenant tous les marqueurs d'une liste, à condition que l'utilisateur qui
+     *en fait le demande ait le droit de visualiser la liste demandée.
+     *
+     * @param idliste la liste que l'utilisateur souhaite visualiser
+     * @param requestingUser l'identifiant de l'utilisateur effectuant la requête
+     * @return Un ResultSet contenant les marqueurs de la liste.
+     *                      Colonnes (idmarqueur, placeType, latitude, longitude, marqueurName)
+     *
+     * ATTENTION : acces rights check désactivé temporairement. Cause un crash au demarage
+     * @since  2.0
+     * @version 1
+     */
     public ResultSet fetchList(long idliste, long requestingUser)    {
         try {
             Statement stmt = con.createStatement();
-//            if(checkListAccessRight(idliste, requestingUser)) {
+            //if(checkListAccessRight(idliste, requestingUser)) {
                 ResultSet rs = stmt.executeQuery("SELECT idmarqueur, marqueurName FROM listemarqueurs NATURAL JOIN marqueurs WHERE idliste = " + idliste +
                         ";");
-                return rs;
+                return rs;//}
 
         }
         catch(SQLException e) {
@@ -51,7 +70,14 @@ public class ListeController implements Listes {
         }
         return null;
     }
-    /** */
+
+    /** Creation d'une entree dans la table listOwner de la base de données.
+     * @param idutilisateur identifiant de l'utilisateur faisant la demande
+     * @param listName le nom qu l'utilisateur souhaite donner à sa liste
+     * @return l'identifiant de la liste nouvellement créée
+     *
+     * @since 2.0
+     * @version 1*/
     public long createList(long idutilisateur, String listName)   {
         try {
             Statement stmt = con.createStatement();
@@ -59,12 +85,11 @@ public class ListeController implements Listes {
                     "(`idutilisateur`, `listname`)\n" +
                     "VALUES\n" +
                     "(" + idutilisateur + ", \"" + listName + "\");") > 0)    {
-                //Si l'insertion réussie, on continue en ajoutant le marqueur dans la table listmarqueurs
                 String getLastIDQuerry = "SELECT idliste FROM listeOwner ORDER BY idliste DESC LIMIT 1;";
                 ResultSet rs = stmt.executeQuery(getLastIDQuerry); //Recuperation de l'id de la liste tout juste crée
                 rs.next();
                 con.commit();
-                return rs.getLong(1);
+                return rs.getLong(1); //renvoi l'identifiant de la liste nouvellement créée
             }
             stmt.close();
         }
@@ -78,10 +103,17 @@ public class ListeController implements Listes {
     /**Cette méthode permet d'ajouter un marqueur à une liste de marqueurs dans la bse de données. Pour cela, elle
      * verifie d'abord que l'utilisateur qui fait la demande a le droit d'ajouter le maqueur a la liste spécifiée
      * (via la methode checkAccessRight)
-     * */
+     *
+     *
+     * ATTENTION : acces rights check désactivé temporairement. Cause un crash au demarage
+     *
+     * @param idliste la liste que l'utilisateur souhaite modifier
+     * @param requestingUser l'identifiant de l'utilisateur effectuant la requête
+     * @param idmarqueur le marqueur a ajouter
+     * @return true en cas de reussite, false en cas d'echec*/
     public boolean addToList(long idmarqueur, long idliste, long requestingUser) {
         try {
-//            if(checkListAccessRight(idliste, requestingUser)) {
+            //if(checkListAccessRight(idliste, requestingUser)) { //verification des droits de l'utilisateur sur la liste
                 Statement stmt = con.createStatement();
                 if(stmt.executeUpdate("INSERT INTO `ourmapdb`.`listemarqueurs`\n" +
                         "(`idliste`,\n" +
@@ -92,6 +124,7 @@ public class ListeController implements Listes {
                     con.commit();
                     return true;
                 }
+                //}
 
             else System.out.println("Erreur lors de l'ajout du collaborateur.");
         }
@@ -101,9 +134,19 @@ public class ListeController implements Listes {
         return false;
     }
 
+    /** Cette méthode permet de donner le statut de collaborateur d'une à un utilisateur.
+     * Elle procède à une vérification  pour savoir si l'utilisateur qui fait la demande est bien propriétaire de la liste.
+     * Seul ce propriétaire peut ajouter un un nouveau collaborateur à la liste.
+     * @param idliste identifiant de la liste en question
+     * @param newCollaborateurID identifiant du nouveau collaborateur
+     * @param requestingUser identifiant de l'utilisateur qui fait
+     * @return true si l'ajout réussi, false si il échoue.
+     *
+     * NON IMPLEMENTEE
+     */
     public boolean addCollaborateurListe(long idliste, String newCollaborateurID, long requestingUser)    {
         try {
-            if(checkOwnerPrivilege(idliste, requestingUser)) {
+            if(checkOwnerPrivilege(idliste, requestingUser)) {//verifie que l'utilisateur est propriétaire de la liste
                 Statement stmt = con.createStatement();
                 if (stmt.executeUpdate("INSERT INTO `ourmapdb`.`collabliste`\n" +
                         "(`idliste`,\n" +
@@ -112,14 +155,14 @@ public class ListeController implements Listes {
                         "(" + idliste + ",\n" +
                         "\"" + newCollaborateurID + "\");") > 0)    {
                     con.commit();
-                    return true;
+                    return true; //réussite
                 }
             }
         }
         catch(SQLException e) {
             e.printStackTrace();
         }
-        return false;
+        return false; //echec
     }
 
     /** Cette méthode vérifie qu'un utilisateur a accès à une liste de marqueurs.
@@ -131,27 +174,28 @@ public class ListeController implements Listes {
      * @param idutilisateur identifiant de l'utilisateur
      * @return true si l''utilisateur a le droit d'acceder à la liste, non sinon
      * @exception SQLException
-     * @author Bastien
+     *
      * */
     public boolean checkListAccessRight(long idliste, long idutilisateur)    {
         try {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT isAdmin FROM utilisateurs WHERE idutilisateur = " + idutilisateur +";");
-            if(rs.getBoolean(1))   return true;
+            if(rs.getBoolean(1))   return true; //la colonne is admin contient soit un 0, soit un 1. 1 Si l'utilisateur est admin
 
             rs = stmt.executeQuery("SELECT idutilisateur FROM listeOwner WHERE idliste = " + idliste +
-                    ";");
+                    ";"); //regarde l'identifiant du propriétaire de la liste dans la base de donnée
             rs.next();
-            if(rs.getLong(1) == idutilisateur)    {
+            if(rs.getLong(1) == idutilisateur)    { //comparaison  avec l'identifiant d'utilisateur donné en paramètre
                 stmt.close();
                 return true;
             }
             else {
                 rs = stmt.executeQuery("SELECT idutilisateur FROM collabliste WHERE idliste = " + idliste +
-                        ";");
+                        ";"); //Consulte la liste des collaborateurs à la liste.
                 stmt.close();
                 while (rs.next()) {
                     if (rs.getLong(1) == idutilisateur) return true;
+                    //Si l'identifiant de l'utilisateur donné en parametre apparait dans la liste, accès autorisé
                 }
             }
         }
@@ -166,9 +210,10 @@ public class ListeController implements Listes {
         try {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT idutilisateur FROM listeOwner WHERE idliste = " + idliste +
-                    ";");
+                    ";");//regarde l'identifiant du propriétaire de la liste dans la base de donnée
             rs.next();
-            if (rs.getLong(1) == idutilisateur) {
+            if (rs.getLong(1) == idutilisateur) {/*Comparaison  avec l'identifiant d'utilisateur donné en
+            paramètrela colonne isAdmin contient soit un 0, soit un 1. 1 Si l'utilisateur est admin*/
                 stmt.close();
                 return true;
             }
